4e5de3fb50375d504dc0e086d9e6ba74693488c6
sergiosergio2 <vzdor5555@gmail.com>
2021-11-22 16:42:48 +0200

    gallery works

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+galleryRef.insertAdjacentHTML('beforeend', creatGalleryToHtml());
+let lightbox = new SimpleLightbox('.gallery a', {captionDelay: 250, captionData: 'alt'});
+
+function addGalleryItemHtml(item) {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+    <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}" /> </a> </li>`;
+
+}
+
+function creatGalleryToHtml () {
+    //console.log(galleryItems);
+    return galleryItems.map(addGalleryItemHtml).join("");
+     
+    
+}
